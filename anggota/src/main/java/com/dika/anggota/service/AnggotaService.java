202d52c6c7bebb2f36b017bacedd864044595136
Dika Jefrianto <145889310+DikaJefrianto@users.noreply.github.com>
2025-09-14 14:47:30 +0700

    Upload Tugas Pertama

---
+++ anggota/src/main/java/com/dika/anggota/service/AnggotaService.java
@@ -0,0 +1,29 @@
+package com.dika.anggota.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dika.anggota.model.Anggota;
+import com.dika.anggota.repository.AnggotaRepository;
+
+@Service
+public class AnggotaService {
+    @Autowired
+    private AnggotaRepository anggotaRepository;
+
+    public List<Anggota> getAllAnggota(){
+        return anggotaRepository.findAll();
+    }
+    public Anggota getAnggotaById(Long id){
+        return anggotaRepository.findById(id).orElse(null);
+    }
+    public Anggota createAnggota(Anggota anggota){
+        return anggotaRepository.save(anggota);
+    }
+    public void deleteAnggota (Long id){
+        anggotaRepository.deleteById(id);
+    }
+}
+
---
+++ pengembalian_service/src/main/java/com/dika/pengembalian_service/repository/PengembalianRepository.java
@@ -0,0 +1,11 @@
+package com.dika.pengembalian_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dika.pengembalian_service.model.Pengembalian;
+
+@Repository
+public interface PengembalianRepository extends JpaRepository<Pengembalian, Long> {
+
+}
